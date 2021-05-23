75156526ffa15396d33216a490207e1d88e9d96b
Ivan <antipchel@gmail.com>
2021-05-23 15:03:12 +0300

    404

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,29 @@
 import React from 'react';
+import {
+  Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Index from './Index.jsx';
+import Login from './Login.jsx';
+import Code404 from './Code404.jsx';
 
-export default () => <h1>Hello world!</h1>;
+export default () => {
+  return <Router>
+  <Switch>
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/">
+      <Index />
+    </Route>
+    <Route path="/404">
+      <Code404 />
+    </Route>
+    <Route path="*">
+      <Redirect to="/404" />
+    </Route>
+  </Switch>
+  </Router>;
+};
---
+++ src/components/Index.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <h1>Hello world! Index</h1>
