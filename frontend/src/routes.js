5d68264cbb157834818617d666e8287393614d45
botirk <14036046+botirk@users.noreply.github.com>
2024-10-28 16:27:20 +0300

    added file

---
+++ frontend/src/routes.js
@@ -0,0 +1,11 @@
+const exp = {
+  login: '/login',
+  signup: '/signup',
+  index: '/',
+  apiLogin: '/api/v1/login',
+  apiSignup: '/api/v1/signup',
+  apiChannels: '/api/v1/channels',
+  apiMessages: '/api/v1/messages',
+};
+
+export default exp;
